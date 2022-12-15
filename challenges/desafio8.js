a370411185671c50dd4acf266603b5b72cd1e813
Icaro Joel <icaromoura.programador@gmail.com>
2022-12-15 15:08:14 -0300

    fix: eslint

--- challenges/desafio8.js
+++ challenges/desafio8.js
@@ -1,2 +1,2 @@
-db.produtos.deleteMany({ curtidas: { $lt: 50 } })
-db.produtos.find({}, { _id: false, nome: 1 })
+db.produtos.deleteMany({ curtidas: { $lt: 50 } });
+db.produtos.find({}, { _id: false, nome: 1 });
